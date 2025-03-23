eb3685326814c3676ba1ecdaf290411897db53b8
LEVANIKAPANADZE <kapanadzelevani522@gmail.com>
2025-03-23 23:04:10 +0400

    Making home page

--- app/components/Footer/page.tsx
+++ app/components/Footer/page.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Image from "next/image";
+import img from "../../../public/assets/shared/desktop/image-best-gear.jpg";
 
 export default function Footer() {
   return (
     <footer>
-      <p>�� 2022 Audiophile Ecommerce. All rights reserved.</p>
+      <Image
+        src={img}
+        alt="Image of guy with headphones"
+        width={327}
+        height={300}
+      />
     </footer>
   );
 }
